8ed6969f483e5a73316f29dc92aa6345da42eb25
Rian04CCOK <rian.silva@sptech.school>
2025-11-19 22:11:30 -0300

    banco de dados atualizado e escrito

--- script_InstruMIX.sql
+++
@@ -1,8 +0,0 @@
-USE InstruMIX;
-
-CREATE TABLE Usuario (
-	idUsuario INT PRIMARY KEY,
-    nome VARCHAR (45),
-    email VARCHAR (45) NOT NULL UNIQUE,
-    senha VARCHAR (45)
-);
---
+++ src/database/INSTRUMIX_BANCO_.sql
@@ -0,0 +1,30 @@
+use instrumix;
+
+select * from Usuario;
+
+truncate table Usuario;
+
+CREATE TABLE Usuario (
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    email VARCHAR(45),
+    senha VARCHAR(45),
+    instrumento VARCHAR(45)
+);
+
+ALTER TABLE Usuario ADD COLUMN qtdMusica VARCHAR (45);
+    
+CREATE TABLE Perguntas (
+	idPerguntas INT PRIMARY KEY AUTO_INCREMENT,
+    texto_pergunta VARCHAR (200),
+    resposta VARCHAR (45)
+);
+
+CREATE TABLE Quiz (
+	idQuiz INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR (45),
+    fkPerguntas INT, 
+		CONSTRAINT fkQuizPerguntas
+			FOREIGN KEY (fkPerguntas)
+				REFERENCES Perguntas (idPerguntas)
+);
