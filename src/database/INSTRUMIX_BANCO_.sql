8e0811c899cc6a49700de4130affadff5f688255
Rian04CCOK <rian.silva@sptech.school>
2025-11-27 15:28:46 -0300

    mudança de BD

--- src/database/INSTRUMIX_BANCO_.sql
+++ src/database/INSTRUMIX_BANCO_.sql
@@ -1,3 +1,5 @@
+create database instrumix;
+
 use instrumix;
 
 select * from Usuario;
@@ -9,7 +11,8 @@ CREATE TABLE Usuario (
     nome VARCHAR(45),
     email VARCHAR(45),
     senha VARCHAR(45),
-    instrumento VARCHAR(45)
+    instrumento VARCHAR(45),
+    qtdMusica VARCHAR (45)
 );
 
 ALTER TABLE Usuario ADD COLUMN qtdMusica VARCHAR (45);
@@ -27,4 +30,15 @@ CREATE TABLE Quiz (
 		CONSTRAINT fkQuizPerguntas
 			FOREIGN KEY (fkPerguntas)
 				REFERENCES Perguntas (idPerguntas)
+);
+
+CREATE TABLE Tentativa (
+	fkUsuario INT,
+    CONSTRAINT fkUsuarioTantativa
+		FOREIGN KEY (fkUsuario)
+			REFERENCES Usuario (idUsuario),
+    fkQuiz INT,
+    CONSTRAINT fkQuizTentativa 	
+		FOREIGN KEY (fkQuiz)
+			REFERENCES Quiz (idQuiz)
 );
